81a37746da09fa1a3596ec2bf76fc04d5170e8c1
Freddy Stock <fbs9594@rit.edu>
2018-12-20 04:15:38 -0500

    Started Player Implementation
    started to implement player movement and camera. Turning radius is very wide right now and should be tweeked. Additionally Turning radius changes depending on how long the player had moved forward or backward before trying to turn which is a bug that needs to be fixed.

---
+++ DarkLight Dungeon/Assets/Scripts/Character.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+    //Movement fields
+    public float mass;
+    public float maxSpeed;
+    public Vector3 targetPosition;
+    public Vector3 direction = new Vector3(1, 0, 0);  
+    public Vector3 velocity = new Vector3(0, 0, 0);
+    public Vector3 acceleration = new Vector3(0, 0, 0);
+    protected Vector3 right;
+    protected Vector3 forward;
+    protected Vector3 futurePos;
+    protected Vector3 pursueForce;
+    protected Vector3 seekingForce;
+    protected Vector3 desiredVelocity;
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+        if(direction == Vector3.zero)
+        {
+            direction = new Vector3(1, 0, 0);
+        }
+
+        forward = direction;
+        right = Quaternion.Euler(0, -90, 0) * direction;
+
+
+    }
+
+    /// <summary>
+    /// applies a force to an object based on its mass
+    /// </summary>
+    /// <param name="force">force to apply</param>
+    protected void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+
+
+    /// <summary>
+    /// move object based on forces
+    /// </summary>
+    protected virtual void Move()
+    {
+        acceleration = acceleration.normalized * maxSpeed;
+        velocity += acceleration * Time.deltaTime;
+
+        transform.position += velocity * Time.deltaTime;
+
+        direction = velocity.normalized;
+        acceleration = Vector3.zero;
+    }
+
+    /// <summary>
+    /// move object towards a target
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    /// <returns></returns>
+    protected Vector3 Seek(Vector3 targetPosition)
+    {
+        desiredVelocity = new Vector3(targetPosition.x - transform.position.x, 0, targetPosition.z - transform.position.z);
+
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+        seekingForce = desiredVelocity - velocity;
+
+        return seekingForce;
+    }
+
+    /// <summary>
+    /// makes object move away from target
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    /// <returns></returns>
+    protected Vector3 Flee(Vector3 targetPosition)
+    {
+        desiredVelocity = -new Vector3(targetPosition.x - transform.position.x, 0, targetPosition.z - transform.position.z);
+
+        desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+        seekingForce = desiredVelocity - velocity;
+
+        return seekingForce;
+    }
+
+    /// <summary>
+    /// void method that calculates movement of the agent
+    /// </summary>
+    public abstract void CalcSteeringForces();
+
+
+
+    //fields
+    protected int health;
+    protected int damage;
+    protected bool invulnerable;
+    protected bool hit;
+
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public int Damage
+    {
+        get { return damage; }
+    }
+
+    public bool Invulnerable
+    {
+        get { return invulnerable; }
+        set { invulnerable = value; }
+    }
+
+    public bool Hit
+    {
+        get { return hit; }
+        set { hit = value; }
+    }
+
+    //methods
+    public virtual void TakeDamage(Player damaged, Character damager)
+    {
+        damaged.Health -= damager.Damage;
+    }
+
+    /// <summary>
+    /// method for determining knockback
+    /// </summary>
+    /// <param name="attacker">character that damaged the character getting knocked backwards</param>
+    public virtual void Knockback()
+    {
+        //write new knockback method or refactor old one 
+    }
+}
+
---
+++ DarkLight Dungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	new void Update () {
+
+        base.Update();
+
+        CalcSteeringForces();
+        Move();
+        transform.LookAt(direction + transform.position);
+	}
+
+    /// <summary>
+    /// moves player based on player inputs and external forces
+    /// </summary>
+    public override void CalcSteeringForces()
+    {
+        ApplyForce(PlayerInputForce());
+    }
+
+    protected override void Move()
+    {
+        acceleration = acceleration.normalized * maxSpeed;
+
+        velocity += acceleration * Time.deltaTime;
+
+        transform.position += acceleration * Time.deltaTime;
+
+        Debug.Log(acceleration);
+
+        direction = velocity.normalized;
+        acceleration = Vector3.zero;
+
+    }
+
+    private Vector3 PlayerInputForce()
+    {
+        Vector3 inputVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector -= forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector -= right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += right;
+        }
+        //if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        //{
+        //    velocity = Vector3.zero;
+        //}
+
+        return inputVector;
+    }
+}
