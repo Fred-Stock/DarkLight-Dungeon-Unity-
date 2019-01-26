19e781e7bda06971e87aeee8c1d584f43e72cc85
Freddy Stock <fbs9594@rit.edu>
2019-01-25 19:36:10 -0500

    Working on breakable rocks
    renamed children of the rock prefab and created a rock script. Click on the rock to break pieces off.

---
+++ DarkLight Dungeon/Assets/Scripts/Rock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+    int iteration;//varaible to track which rock to break off
+
+	// Use this for initialization
+	void Start () {
+        iteration = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseDown()
+    {
+        iteration++;
+        CrumbleRock();
+    }
+
+    /*
+     * loop that iterates when player clicks on rock
+     * Each time it iterates break of corresponding rock
+     */
+
+    void CrumbleRock()
+    {
+        
+        Transform child = transform.Find("shard" + iteration);
+        child.gameObject.AddComponent<Rigidbody>();
+        child.GetComponent<Rigidbody>().drag = .01f;
+        child.GetComponent<Rigidbody>().mass = 20;
+        
+    }
+}
